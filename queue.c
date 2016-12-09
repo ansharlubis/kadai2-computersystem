18757227743ab4d2ba399b8a006123fb1bf8b3b0
Luthfan Anshar Lubis <lubis.l.aa@m.titech.ac.jp>
2016-12-09 19:17:41 +0900

    buat report

--- queue.c
+++ queue.c
@@ -11,10 +11,15 @@ int put(struct node *head, int val){
 	new = (struct node *)malloc(sizeof(struct node));
 	if (new == NULL) return -1;
 
-	new -> val = head -> val;
-	new -> next = head -> next;
-	head -> val = val;
-	head -> next = new;
+	if (head -> val == -1){
+		head -> val = val;
+		head -> next = NULL;
+	} else {
+		new -> val = head -> val;
+		new -> next = head -> next;
+		head -> val = val;
+		head -> next = new;
+	}
 
 	return 0;
 }
@@ -24,12 +29,13 @@ int get(struct node *head){
 
 	if (t == NULL) return -1;
 
-	while(t -> next != NULL){
+	while((t -> next) -> next != NULL){
 		t = t -> next;
 	}
 
-	int p = t -> val;
-	free(t);
+	int p = (t -> next) -> val;
+	t -> next = NULL;
+	free(t -> next);
 
 	return p;
 	
@@ -63,13 +69,42 @@ int delete(struct node *head, int val){
 void display(struct node *head){
 	struct node *t = head;
 
-	while(t -> next != NULL){
-		display(t -> next);
-		printf("%d ", t -> val);
+	if(head -> next == NULL){
+		printf("%d ", head -> val);
+	} else {
+		display(head -> next);
+		printf("%d ", head -> val);
 	}
 }
 
 int main(){
-	
-	return 0;
+	struct node head = {-1, NULL};
+	int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i, res;
+
+	for(i = 0; i < 10; i++){
+		printf("put %d\n", nums[i]);
+		res = put(&head, nums[i]);
+		if(res != 0) return 1;
+	}
+
+	printf("queue: ");
+	display(&head);
+	printf("\n");
+
+	for (i = 0; i < 3; i++){
+		res = get(&head);
+		printf("get %d\n", res);
+	}
+
+	printf("queue: ");
+	display(&head);
+	printf("\n");
+
+	res = delete(&head, 7);
+	printf("delete %d\n", res);
+
+	printf("queue: ");
+	display(&head);
+	printf("\n");
 }
